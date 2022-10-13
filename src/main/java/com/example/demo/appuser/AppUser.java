44afda37ef6833c7f35ef63ca672d439ad472709
avijeetshil110 <shil.avijeet@revesoft.com>
2022-10-13 12:50:12 +0600

    AppUser Entity constructed

--- src/main/java/com/example/demo/appuser/AppUser.java
+++ src/main/java/com/example/demo/appuser/AppUser.java
@@ -1,19 +1,37 @@
 package com.example.demo.appuser;
 
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.persistence.*;
 import java.util.Collection;
 import java.util.Collections;
 
-public class appUser implements UserDetails {
 
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@Entity
+public class AppUser implements UserDetails {
+    @Id
+    @SequenceGenerator(
+            name="student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String username;
     private String email;
     private String password;
+    @Enumerated(EnumType.STRING)
     private AppUserRole appUserRole;
     private  Boolean locked;
     private Boolean enabled;
@@ -52,5 +70,20 @@ public class appUser implements UserDetails {
     @Override
     public boolean isEnabled() {
         return enabled;
+    }
+    public AppUser(String name,
+                   String username,
+                   String email,
+                   String password,
+                   AppUserRole appUserRole,
+                   Boolean locked,
+                   Boolean enabled) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.appUserRole = appUserRole;
+        this.locked = locked;
+        this.enabled = enabled;
     }
 }
