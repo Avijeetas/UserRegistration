587227eb6fc3ba456590ca9f5d92434d8e2a6103
avijeetshil110 <shil.avijeet@revesoft.com>
2022-10-17 16:01:44 +0600

    add class to validate email

--- src/main/java/com/example/demo/registration/RegistrationService.java
+++ src/main/java/com/example/demo/registration/RegistrationService.java
@@ -4,8 +4,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class RegistrationService {
-
+    private EmailValidator emailValidator;
     public String register(RegistrationRequest req) {
+
+        boolean isValidEmail=emailValidator.
+                test(req.getEmail());
+        if(!isValidEmail)   throw new IllegalStateException(("email not found"));
+
         return  "works";
     }
 }
