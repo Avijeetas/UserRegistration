f41c860cf1494c18be8f55d13ec4ee666de9d9bc
avijeetshil <avijeetshil110@gmail.com>
2022-10-18 22:17:15 +0600

    added maildev

--- src/main/java/com/example/demo/registration/RegistrationService.java
+++ src/main/java/com/example/demo/registration/RegistrationService.java
@@ -39,7 +39,7 @@ public class RegistrationService {
                 )
         );
 
-        String link = "localhost:8080/api/v1/registration/confirm?token=" + token;
+        String link = "http://localhost:8080/api/v1/registration/confirm?token=" + token;
         emailSender.send(
                 req.getEmail(),
                 buildEmail(req.getFirstName(), link));
--- src/main/java/com/example/demo/registration/email/EmailService.java
+++ src/main/java/com/example/demo/registration/email/EmailService.java
@@ -27,6 +27,7 @@ public class EmailService implements    EmailSender{
             helper.setTo(to);
             helper.setSubject("Confirm your email");
             helper.setFrom("india12tour@gmail.com");
+            mailSender.send(mimeMessage);
         }
         catch (MessagingException e){
             LOGGER.error("failed to send email ",e);
