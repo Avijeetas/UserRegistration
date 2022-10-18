7837dc0c6a4c0fce01d415bdd0f260b0a49d8484
avijeetshil <avijeetshil110@gmail.com>
2022-10-18 13:21:07 +0600

    added functionality to create construction from request

--- src/main/java/com/example/demo/registration/RegistrationService.java
+++ src/main/java/com/example/demo/registration/RegistrationService.java
@@ -1,16 +1,35 @@
 package com.example.demo.registration;
 
+import com.example.demo.appuser.AppUser;
+import com.example.demo.appuser.AppUserRole;
+import com.example.demo.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
-    private EmailValidator emailValidator;
+
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
+
+
+
     public String register(RegistrationRequest req) {
 
         boolean isValidEmail=emailValidator.
                 test(req.getEmail());
         if(!isValidEmail)   throw new IllegalStateException(("email not found"));
 
-        return  "works";
+        return appUserService.signUpUser(
+                new AppUser(
+                        req.getFirstName(),
+                        req.getLastName(),
+                        req.getEmail(),
+                        req.getPassword(),
+                        AppUserRole.USER
+
+                )
+        ) ;
     }
 }
