07876e3c9bb35a5dc844f17a09900776185ce911
avijeetshil110 <shil.avijeet@revesoft.com>
2022-10-19 11:49:10 +0600

    email validator

--- src/main/java/com/example/demo/registration/EmailValidator.java
+++ src/main/java/com/example/demo/registration/EmailValidator.java
@@ -4,13 +4,22 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Predicate;
+import java.util.regex.Pattern;
+
 @Service
 @AllArgsConstructor
 public class EmailValidator implements Predicate<String> {
 
+
+    public static boolean patternMatches(String email, String regex){
+        return Pattern.compile(regex)
+                .matcher(email).matches();
+    }
     @Override
     public boolean test(String s) {
         //TODO : Regex to validate email
-        return true;
+        String regexPattern = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+
+        return patternMatches(s, regexPattern);
     }
 }
