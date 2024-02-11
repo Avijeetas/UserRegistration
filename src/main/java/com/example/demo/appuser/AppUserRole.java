61aad6cf3baf640f287b96b5c4765d491d0bb870
Avijeet Shil <avijeetshil110@gmail.com>
2024-02-10 23:46:10 -0600

    added AppUserRole

---
+++ src/main/java/com/example/demo/appuser/AppUserRole.java
@@ -0,0 +1,12 @@
+package com.example.demo.appuser;
+
+/**
+ * @author Avijeet
+ * @project UserRegistration
+ * @github avijeetas
+ * @date 02-10-2024
+ **/
+public enum AppUserRole {
+   USER,
+   ADMIN,
+}
